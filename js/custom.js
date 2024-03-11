d09d15a0ddc10195b2d779ea68314e8afd3c5e7e
Modabbir Mossen <125083879+w3modabbir@users.noreply.github.com>
2024-03-11 12:51:36 +0600

    calender all done

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@ const weekDay = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Sa
 
 const allMonth = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
-date.innerHTML = today.getDate();
+date.innerHTML = (today.getDate()<10? "0" : "") + today.getDate();
 day.innerHTML = weekDay[today.getDay()];
 month.innerHTML = allMonth[today.getMonth()];
 year.innerHTML = today.getFullYear();
